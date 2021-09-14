4bb707aecbaa0e5d6cd09d650dd106433fe0023f
Richard Easton <richeaston@users.noreply.github.com>
2021-09-14 13:24:45 +0100

    Update Teamviewer-Detection.ps1
    Corrected Typo's

--- Teamviewer-Detection.ps1
+++ Teamviewer-Detection.ps1
@@ -1,16 +1,16 @@
 Try {
     if (Get-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" | Where-Object {$_.DisplayName -Like "TeamViewer*"}) {
-        Write-output "Teamveiwer Installer"
+        Write-output "TeamViewer Installer"
         Exit 1
     } else {
-        Write-output "TeamVeiwer not Installer"
+        Write-output "TeamViewer not Installer"
         Exit 0
     }
      if (Get-ItemProperty "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" | Where-Object {$_.DisplayName -Like "TeamViewer*"}) {
-        Write-output "Teamveiwer Installer"
+        Write-output "TeamViewer Installer"
         Exit 1
     } else {
-        Write-output "TeamVeiwer not Installer"
+        Write-output "TeamViewer not Installer"
         Exit 0
     }
 }
