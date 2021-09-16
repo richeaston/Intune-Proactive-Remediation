efaecf4feed18e4f005519e6af6165ecf3612677
Richard Easton <richeaston@users.noreply.github.com>
2021-09-16 13:31:04 +0100

    Add files via upload

---
+++ MSEdge-Forced-Update/MSEDGE Remediation.ps1
@@ -0,0 +1,11 @@
+﻿start MSEDGE
+Sleep 1
+$wshell = New-Object -ComObject wscript.shell; # shell for sending keys
+$wshell.AppActivate('MSEDGE') # make sure chrome is the active window
+Sleep 1
+$wshell.SendKeys('edge://settings/help')
+Sleep .5
+$wshell.SendKeys("{ENTER}")
+#Sleep 300 # sleep to allow updates to be processed 
+# change sleep values if computer is slow.
+Exit 0
---
+++ MSEdge-Forced-Update/MSEDGE detection.ps1
@@ -0,0 +1,40 @@
+﻿try { 
+        Clear
+        #check MSEDGE version installed    
+        $EdgeVersionInfo = (Get-AppxPackage -Name "Microsoft.MicrosoftEdge.Stable").Version
+        Write-output "Installed MSEDGE Version: $EdgeVersionInfo" 
+
+        #Get latest version of MSEDGE
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        $j = Invoke-WebRequest 'https://edgeupdates.microsoft.com/api/products?view=enterprise' | ConvertFrom-Json
+
+        foreach ($ver in $j) {
+            #$channel = $ver.Product 
+            if ($ver.Product -like 'stable' ) {
+                foreach ($v in $(($ver.Releases).ProductVersion[0])) {
+                    if ($v -eq $EdgeVersionInfo ) {
+                        #version installed is latest
+                        Write-output "Stable Version: $v,  MSEDGE is the latest stable release"
+                        Exit 0
+                    } else {
+                        #version installed is not latest
+                        Write-output "Stable Version:$v, Installed Version $EdgeVersionInfo, Not safe, trigger alert" 
+                        Exit 1
+                    }
+                }
+            }
+        }
+}
+catch {
+    $errMsg = $_.Exception.Message
+    if ($errmsg -eq "Cannot bind argument to parameter 'Path' because it is null.") {
+        write-output "MSEDGE does not appear to be installed"
+        Exit 0
+    } else {
+        Write-output $errMsg
+        Exit 1
+    }
+}
+
+
+
