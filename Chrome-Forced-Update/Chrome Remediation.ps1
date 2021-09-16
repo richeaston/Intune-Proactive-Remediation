73794c8a65553e9f168c4f40a9a8af7414ed8a1e
Richard Easton <richeaston@users.noreply.github.com>
2021-09-16 13:30:35 +0100

    Add files via upload

---
+++ Chrome-Forced-Update/Chrome Remediation.ps1
@@ -0,0 +1,11 @@
+﻿start chrome
+Sleep 1
+$wshell = New-Object -ComObject wscript.shell; # shell for sending keys
+$wshell.AppActivate('Chrome') # make sure chrome is the active window
+Sleep 1
+$wshell.SendKeys('chrome://settings/help')
+Sleep .5
+$wshell.SendKeys("{ENTER}")
+#Sleep 300 # sleep to allow updates to be processed 
+# change sleep values if computer is slow.
+Exit 0
