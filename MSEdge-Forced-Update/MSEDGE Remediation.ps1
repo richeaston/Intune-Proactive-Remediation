8dfdca59fc9435423653de698e7e39dcdefd90df
Richard Easton <richeaston@users.noreply.github.com>
2021-12-10 10:38:09 +0000

    Update Active window code
    Change the show-window from wshell to whsell wrapped in powershell, as it's more reliable

--- MSEdge-Forced-Update/MSEDGE Remediation.ps1
+++ MSEdge-Forced-Update/MSEDGE Remediation.ps1
@@ -1,11 +1,30 @@
-﻿start MSEDGE
-Sleep 1
+clear-host
+
+function Show-Window {
+  param(
+    [Parameter(Mandatory)]
+    [string] $ProcessName
+  )
+
+  $ProcessName = $ProcessName -replace '\.exe$'
+  $procId = (Get-Process -ErrorAction Ignore $ProcessName).Where({ $_.MainWindowTitle }, 'First').Id
+
+  if (-not $procId) { Throw "No $ProcessName process with a non-empty window title found." }
+  $null = (New-Object -ComObject WScript.Shell).AppActivate($procId)
+
+}
+
+
+Start-Process "MSEDGE.exe"
+Start-Sleep -seconds 3
 $wshell = New-Object -ComObject wscript.shell; # shell for sending keys
-$wshell.AppActivate('MSEDGE') # make sure chrome is the active window
-Sleep 1
-$wshell.SendKeys('edge://settings/help')
-Sleep .5
-$wshell.SendKeys("{ENTER}")
-#Sleep 300 # sleep to allow updates to be processed 
-# change sleep values if computer is slow.
-Exit 0
+
+    Start-Sleep 3
+    Show-Window MSEDGE
+    Start-Sleep -milliseconds 500
+    $wshell.SendKeys('edge://settings/help')
+    Start-Sleep -milliseconds 500
+    $wshell.SendKeys("{ENTER}")
+    Start-sleep -Milliseconds 300
+    Exit 0
+    
